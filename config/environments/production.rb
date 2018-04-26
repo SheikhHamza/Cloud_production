4c51c58d7d312c621aed8d2060221436a5067155
Sheikh Hamza <hamzasajjaditya@gmail.com>
2018-04-26 17:11:07 +0500

    add untracted files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,10 +91,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('sheikhcloudproject'),
-    access_key_id: ENV.fetch('AKIAJMWIA2F7A4EHWU7A'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('US East (N. Virginia)'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
-} 
+}
 end
